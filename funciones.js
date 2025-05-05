8c68e23f2f131cde7fa00d07c26dac6226201b5e
Marlon0515 <mamosqueraq@unal.edu.co>
2025-05-05 15:22:11 -0500

    feat:creacion de botones

---
+++ funciones.js
@@ -0,0 +1,11 @@
+function init(){
+    var productos = document.getElementById("productos");
+    productos=productos.innerHTML=
+    `<div> 
+        <label> ProductoA </label> 
+        <input min="1" type="number" name="" id=""> 
+        <button class="btn btn-primary" type = "button">Agregar</button>
+
+    </div> `;
+
+}
